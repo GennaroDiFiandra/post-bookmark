4dd34b2c2c59a07b4be29b40703921f7cb39fb26
Gennaro Di Fiandra <gennarodifiandra@gmail.com>
2024-04-17 19:14:28 +0200

    Enhance class reusability

--- includes/TranslationsLoader.php
+++ includes/TranslationsLoader.php
@@ -6,9 +6,11 @@ defined('WPINC') || die;
 
 class TranslationsLoader
 {
+  private const TEXTDOMAIN = 'post-bookmark';
+
   public function load_translations()
   {
-    load_plugin_textdomain('post-bookmark', false, 'post-bookmark/languages');
+    load_plugin_textdomain(self::TEXTDOMAIN, false, self::TEXTDOMAIN.'/languages');
   }
 
   public function setup_hooks()
